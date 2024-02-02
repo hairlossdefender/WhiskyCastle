9b6ceddbd367a898b38a8c8ffcab63b70515f8da
hairlossdefender <jjh63883417@gmail.com>
2024-02-02 11:33:57 +0900

    데이터베이스 연동됨

--- WhiskyCastle/WhiskLV1/WPR.java
+++ WhiskyCastle/WhiskLV1/WPR.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
-
+import java.sql.*;
 
 //
 
@@ -13,11 +13,23 @@ public class WPR {
 
 	public static void main(String[] args) {
 	    // TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/whiskycastle","root","Zxczxc123123@");
+			Statement stat=con.createStatement();
+			System.out.println("insert");
+			String sql="Insert into castleID values(01)";
+			stat.execute(sql);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		/*
 	   ArrayList<Whisky> whiskies;
 	   Options option = new Options();
 	   whiskies=option.archivist();
 	   option.getWhisky(2);
 	   option.SortWhiskies(whiskies);
+	   */
 	}
 }
    
--- WhiskyCastle/WhiskLV1/Whisky.java
+++ WhiskyCastle/WhiskLV1/Whisky.java
@@ -1,10 +1,13 @@
 package WhiskLV1;
 
+import java.util.HashMap;
+
 public class Whisky {
 	  
 		private String name;
 	    private int score;
 	    private double vol;
+	    private HashMap<String, String> tastingNote = new HashMap<>();
 	    public Whisky(String name, int score, double vol) {
 	        this.name = name;
 	        this.score = score;
